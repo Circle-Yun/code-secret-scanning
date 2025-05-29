fe6070b2b11b348978603113adf8b649ea2da487
yunhwan <a57710465@gmail.com>
2025-05-29 16:43:34 +0900

    add vulnerable code

---
+++ demo.py
@@ -0,0 +1,9 @@
+# demo.py
+
+import subprocess
+
+def insecure(): # insecure 함수 정의
+cmd = input("Enter command: ")
+subprocess.call(cmd, shell=True) # ⚠️ Command Injection 가능성
+
+insecure() # insecure 함수 실행
